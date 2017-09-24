03a02192b880af9ba2b58e866cb625ca6eb7a3c1
doubleandy <doubleandy@gmail.com>
2017-09-23 21:01:01 -0700

    add post route

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-  id: 1,
-  description: 'Take out trash',
-  completed: false
-}, {
-  id: 2,
-  description: 'Go shopping',
-  completed: false
-}]
+var todos = [];
+var nextTodoId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.send('Todo API Root');
@@ -35,6 +32,13 @@ app.get('/todos/:id', function(req, res) {
     res.status(404).send();
   }
 
+});
+
+app.post('/todos', function (req, res) {
+  var body = req.body;
+  body.id = nextTodoId++;
+  todos.push(body);
+  res.json(body);
 })
 
 app.listen(PORT, function(argument) {
