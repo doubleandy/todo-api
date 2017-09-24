e1da48adc2edbcb6efd05550a8f45c47a6dd7d5a
doubleandy <doubleandy@gmail.com>
2017-09-23 20:09:50 -0700

    Add /todos/:id route

--- server.js
+++ server.js
@@ -17,8 +17,26 @@ app.get('/', function(req, res) {
 
 app.get('/todos', function(req, res) {
   res.json(todos);
+});
+
+app.get('/todos/:id', function(req, res) {
+  var matchedTodoId;
+  var todoId = parseInt(req.params.id, 10);
+
+  for(i = 0; i < todos.length; i++) {
+    if (todoId === todos[i].id) {
+      matchedTodoId = todos[i];
+    }
+  }
+
+  if (matchedTodoId) {
+    res.json(matchedTodoId);
+  } else {
+    res.status(404).send();
+  }
+
 })
 
 app.listen(PORT, function(argument) {
   console.log('Express listening on port ' + PORT + '!');
-})
+});
