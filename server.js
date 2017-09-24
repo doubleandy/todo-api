6cc8efa9ca8937e3e445adbd363fa0cb62a07fb2
doubleandy <doubleandy@gmail.com>
2017-09-23 19:26:41 -0700

    Add /todos route

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+  id: 1,
+  description: 'Take out trash',
+  completed: false
+}, {
+  id: 2,
+  description: 'Go shopping',
+  completed: false
+}]
 
 app.get('/', function(req, res) {
   res.send('Todo API Root');
 });
+
+app.get('/todos', function(req, res) {
+  res.json(todos);
+})
 
 app.listen(PORT, function(argument) {
   console.log('Express listening on port ' + PORT + '!');
