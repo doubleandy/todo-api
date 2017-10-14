c8fc45944ef971f578c6152703f7f54a73107e93
doubleandy <doubleandy@gmail.com>
2017-10-14 11:57:59 -0700

    Hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,5 +1,9 @@
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
 module.exports = function (sequelize, DataTypes) {
-  return sequelize.define('user', {
+  // return sequelize.define('user', {
+  const User =  sequelize.define('user', {
     email: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -8,20 +12,41 @@ module.exports = function (sequelize, DataTypes) {
         isEmail: true
       }
     },
+    salt: {
+      type: DataTypes.STRING
+    },
+    password_hash: {
+      type: DataTypes.STRING
+    },
     password: {
-      type: DataTypes.STRING,
+      type: DataTypes.VIRTUAL,
       allowNull: false,
       validate: {
         len: [7,100]
+      },
+      set: function(value) {
+        var salt = bcrypt.genSaltSync(10);
+        var hashedPassword = bcrypt.hashSync(value, salt);
+
+        this.setDataValue('password', value);
+        this.setDataValue('salt', salt);
+        this.setDataValue('password_hash', hashedPassword);
       }
     }
   }, {
     hooks: {
-      beforeValidate: (user, options) => {
+      beforeValidate: function(user, options)  {
         if(typeof user.email === 'string') {
           user.email = user.email.toLowerCase();
         }
       }
     }
   });
+
+  User.prototype.toPublicJSON = function() {
+    var json = this.toJSON();
+    return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+  };
+
+  return User
 }
